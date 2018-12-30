e1e0086297dd7256270fdc7ac7907c318a175d83
Jimi <y.tlh@hotmail.com>
2018-12-30 02:35:34 -0600

    Se agrego el codigo para pedir permisos, asi como los codigos para enviar los mensajes de cada dato encontrado en el txt

--- app/src/main/java/com/example/jime/smscfe/MainActivity.java
+++ app/src/main/java/com/example/jime/smscfe/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.jime.smscfe;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -31,12 +38,17 @@ public class MainActivity extends AppCompatActivity {
     Button buscar,iniciar;
     ListView lista;
     public static Activity activity;
+    String InfoFacturas [][] = new String[1000][4];
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private final int MY_PERMISSIONS_REQUEST_MENSAJE = 2;
+    ProgressDialog enviando;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buscar = (Button)findViewById(R.id.btBuscar);
         iniciar = (Button)findViewById(R.id.btIniciar);
+        iniciar.setVisibility(View.INVISIBLE);
         lista = (ListView)findViewById(R.id.ListaFacturas);
         activity = this;
 
@@ -44,16 +56,71 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 explorador();
+
             }
         });
 
         iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                enviando = new ProgressDialog(getApplicationContext());
+                enviando.setMessage("Subiendo Supervicion. Por favor espere...");
+                enviando.setIndeterminate(false);
+                enviando.setCancelable(false);
+                enviando.show();
+                IniciaProceso();
             }
         });
 
+        Inicializa();
+        permisosAlmacenamiento();
+        permisoMensajes();
+
+    }
+    public void IniciaProceso(){
+        for(int x = 0;x<1000;x++){
+            try{
+                if(InfoFacturas[x][0].length()>0) {
+                    String mensaje = "Su servicio "+InfoFacturas[x][0]+" A sido facturado con un importe " +
+                            "de: $"+InfoFacturas[x][1]+" pesos, con vencimiento maximo 3 dias posteriores a este mensaje. Visite www.cfe.com";
+                    if(EnviarMensaje(InfoFacturas[x][2],mensaje)) {
+                        InfoFacturas[x][3] = "Enviado";
+                    }
+                }else{
+                    break;
+                }
+            }catch (Exception e){
+
+            }
+        }
+        rellenaLista();
+        enviando.dismiss();
+        enviando.cancel();
+    }
+    private boolean EnviarMensaje (String Numero, String Mensaje){
+        try {
+            if(Mensaje.length()<=155) {
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage(Numero, null, Mensaje, null, null);
+                return true;
+            }else{
+                Toast.makeText(getApplicationContext(), "MENSAJE NO ENVIADO, EL MENSAJE ES MUY LARGO", Toast.LENGTH_LONG).show();
+            }
+
+        }catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ERROR ENVIANDO MENSAJE", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void Inicializa(){
+        for(int x = 0;x<1000;x++){
+            InfoFacturas[x][0] = "";
+            InfoFacturas[x][1] = "";
+            InfoFacturas[x][2] = "";
+            InfoFacturas[x][3] = "";
+        }
     }
 
     public void explorador() {
@@ -62,11 +129,9 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Choose File"), VALOR_RETORNO);
     }
     public void leerTxt(String ruta) {
-        ArrayList<datosFactura> datos = new ArrayList<>();
+
         String linea = "";
-        String lineas[] = new String[10];
-        int conta = 0;
-        String re = "";
+        int contador = 0;
         try {
             File archivo = new File(ruta);
             BufferedReader txt = new BufferedReader(new InputStreamReader(new FileInputStream(archivo.getAbsolutePath())));
@@ -77,15 +142,78 @@ public class MainActivity extends AppCompatActivity {
                 line = linea.substring(line.indexOf("-")+1, line.length());
                 String importe = line.substring(0,line.indexOf("-")).trim();
                 String tel = line.substring(line.indexOf("-")+1, line.length()).trim();
-
-                datosFactura contenido = new datosFactura(rpu,importe,tel,"Pendiente");
-                datos.add(contenido);
+                InfoFacturas[contador][0] = rpu;
+                InfoFacturas[contador][1] = importe;
+                InfoFacturas[contador][2] = tel;
+                InfoFacturas[contador][3] = "Pendiente";
+                contador++;
             }
-            FacturasAdapter adapter = new FacturasAdapter(this,datos);
-            lista.setAdapter(adapter);
+            rellenaLista();
+            if(contador>0){
+                iniciar.setVisibility(View.VISIBLE);
+            }
         } catch (Exception e) {
             System.out.println("Excepcion: " + e.toString());
         }
+    }
+    public void permisosAlmacenamiento(){
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+            }
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+            }
+        }
+    }
+
+    public void permisoMensajes(){
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.SEND_SMS)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_MENSAJE);
+
+            }
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.SEND_SMS)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_MENSAJE);
+
+            }
+        }
+    }
+
+    public void rellenaLista(){
+        ArrayList<datosFactura> datos = new ArrayList<>();
+        for(int x = 0;x<1000;x++){
+            if(InfoFacturas[x][0].length()>0) {
+                datosFactura contenido = new datosFactura(InfoFacturas[x][0], InfoFacturas[x][1], InfoFacturas[x][2], InfoFacturas[x][3]);
+                datos.add(contenido);
+            }else{
+                break;
+            }
+        }
+        FacturasAdapter adapter = new FacturasAdapter(this,datos);
+        lista.setAdapter(adapter);
     }
 
     public static String getFilePath(Context context, Uri uri) throws URISyntaxException {
